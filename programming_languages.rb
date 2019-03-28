beca3cbaf190fc8e378206c83cb72a42b2a45f3f
SOYEONG OH <axionoso@gmail.com>
2019-03-28 03:54:00 +0000

    Automatically backed up by Learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,15 +1,4 @@
 def reformat_languages(languages)
-  # new_hash = {}
-  # languages.each do |style, language|
-  #   language.each do |lang, type|
-  #     if new_hash[lang] == nil
-  #       new_hash[lang] = type
-  #       new_hash[lang][:style] = []
-  #     end
-  #     new_hash[lang][:style] << style
-  #   end
-  # end
-  # new_hash
   new_hash = {}
   languages.each do |style, language|
     language.each do |lang, type|
